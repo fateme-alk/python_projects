0c743015c159822fdb7ae982753c90571dce07c9
fateme-alk <fateme.alk9@gmail.com>
2023-06-30 14:32:07 +0430

    add copy function without using shutil/os.system/os.popen

--- organize_files/organize_files.py
+++ organize_files/organize_files.py
@@ -14,6 +14,12 @@ def create_dir(parent_dir, dir_name):
 def find_file_extension(file_name):
     return (file_name.split('.')[-1]).lower()
 
+def copy_file(src_file_path, dst_file_path):
+    open(dst_file_path, "w")
+    with open(src_file_path, 'rb') as src_file:
+        with open(dst_file_path, 'wb') as dest_file:
+            dest_file.write(src_file.read())
+
 categories = {
     'pic': ('jpg', 'jpeg', 'png'),
     'video': ('mp4', 'avi', '3gp', 'mpeg', 'mkv', 'wmv', 'mov'),
@@ -23,10 +29,13 @@ _, src_folder_path, dst_folder_path = tuple(sys.argv)
 
 for dir_path, dir_names, file_names in os.walk(src_folder_path):
     for file_name in file_names:
-        file_path = os.path.join(dir_path, file_name)
-        file_year = str(modification_date(file_path).tm_year)
+        src_file_path = os.path.join(dir_path, file_name)
+        file_year = str(modification_date(src_file_path).tm_year)
         create_dir(dst_folder_path, file_year)
         file_extension = find_file_extension(file_name)
         for category, extensions_of_category in categories.items():
-            if file_extension in categories[category]:
+            if file_extension in extensions_of_category:
                 create_dir(os.path.join(dst_folder_path, file_year),category)
+                dst_file_path = os.path.join(dst_folder_path, file_year, category, file_name)
+                copy_file(src_file_path, dst_file_path)
+            
